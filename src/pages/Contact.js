d7cd7f926c3893f607e68256643733e3fb74e8a4
Pulkit Gupta <pulkitgupta95700@gmail.com>
2023-05-27 12:09:50 +0530

    done some more changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,6 +9,7 @@ const Contact = () => {
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
   const [number, setNumber] = useState("");
+  const [sendingState, setSendingState] = useState(false);
 
   const formRef = useRef();
 
@@ -16,6 +17,7 @@ const Contact = () => {
   
     e.preventDefault();
 
+    setSendingState(true);
     emailjs
       .sendForm(
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
@@ -29,9 +31,11 @@ const Contact = () => {
         setSubject("");
         setMessage("");
         setNumber("");
+        setSendingState(false);
       })
       .catch((err) => {
         alert("An error occurred, Please try again", err.text);
+        setSendingState(false);
       });
   };
 
@@ -131,7 +135,7 @@ const Contact = () => {
                 type="submit"
                 className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Email us
+                {sendingState ? "Sending..." : "Send Email"}
               </button>
               <div className="text-sm font-medium text-gray-500 dark:text-gray-300"></div>
             </form>
