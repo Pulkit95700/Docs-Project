ee0fa74c41cb4bf9d3753a66bcbb6eb8936eab8e
Pulkit Gupta <pulkitgupta95700@gmail.com>
2023-05-27 23:03:35 +0530

    done more changes

--- src/components/Bars/SlideBar.js
+++ src/components/Bars/SlideBar.js
@@ -116,13 +116,13 @@ const SlideBar = (props) => {
       <div className="mt-14 md:mt-32 self-end flex-1">
         <img src={Logo} className="w-1/2 " />
         <div className="hidden md:block">
-          <p className=" flex gap-4 text-gray-600 text-sm mt-4">
-            <span>Contact:</span>
-            <a href="tel:+8801711111111">+8801711111111</a>
+          <p className=" flex  gap-4 text-gray-600 text-sm mt-4 font-semibold">
+            <span className="w-14 font-semibold text-black">Contact:</span>
+            <a href="tel:+919538544507">+919538544507</a>
           </p>
-          <p className=" flex gap-4 text-gray-600 text-sm mt-2">
-            <span>Email:</span>
-            <a href="mailto:atozsec11@gmail.com">atozsec11@gmail.com</a>
+          <p className=" flex  gap-4 text-gray-600 font-semibold text-sm mt-2">
+            <span className="w-14 font-semibold text-black">Email:</span>
+            <a href="mailto:atozsec11@gmail.com">AtoZSec11@gmail.com</a>
           </p>
         </div>
       </div>
